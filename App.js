d853f2fea05c91658e3245bbddcbb87d5b21c443
Aman Mittal <amandeepmittal@live.com>
2019-03-05 12:22:15 +0530

    🚀

--- App.js
+++ App.js
@@ -1,50 +1,81 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import { Dialogflow_V2 } from 'react-native-dialogflow';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { dialogflowConfig } from './env';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const BOT_USER = {
+	_id: 2,
+	name: 'FAQ Bot',
+	avatar: 'https://i.imgur.com/7k12EPD.png'
+};
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+class App extends Component {
+	state = {
+		messages: [
+			{
+				_id: 1,
+				text: `Hi! I am the FAQ bot 🤖 from Jscrambler.\n\nHow may I help you with today?`,
+				createdAt: new Date(),
+				user: BOT_USER
+			}
+		]
+	};
+
+	componentDidMount() {
+		Dialogflow_V2.setConfiguration(
+			dialogflowConfig.client_email,
+			dialogflowConfig.private_key,
+			Dialogflow_V2.LANG_ENGLISH_US,
+			dialogflowConfig.project_id
+		);
+	}
+
+	handleGoogleResponse(result) {
+		let text = result.queryResult.fulfillmentMessages[0].text.text[0];
+		this.sendBotResponse(text);
+	}
+
+	onSend(messages = []) {
+		this.setState(previousState => ({
+			messages: GiftedChat.append(previousState.messages, messages)
+		}));
+
+		let message = messages[0].text;
+		Dialogflow_V2.requestQuery(
+			message,
+			result => this.handleGoogleResponse(result),
+			error => console.log(error)
+		);
+	}
+
+	sendBotResponse(text) {
+		let msg = {
+			_id: this.state.messages.length + 1,
+			text,
+			createdAt: new Date(),
+			user: BOT_USER
+		};
+
+		this.setState(previousState => ({
+			messages: GiftedChat.append(previousState.messages, [msg])
+		}));
+	}
+
+	render() {
+		return (
+			<View style={{ flex: 1, backgroundColor: '#fff' }}>
+				<GiftedChat
+					messages={this.state.messages}
+					onSend={messages => this.onSend(messages)}
+					user={{
+						_id: 1
+					}}
+				/>
+			</View>
+		);
+	}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default App;
---
+++ env.example.js
@@ -0,0 +1,15 @@
+// put real values generated in JSON file instead of X's
+// also rename the file to "env.js" when in use
+
+export const dialogflowConfig = {
+	type: 'service_account',
+	project_id: 'faq-bot-XXXX',
+	private_key_id: 'XXXX',
+	private_key: '-----BEGIN PRIVATE KEY-----XXXX\n-----END PRIVATE KEY-----\n',
+	client_email: 'XXXX',
+	client_id: 'XXXX',
+	auth_uri: 'XXXX',
+	token_uri: 'XXXX',
+	auth_provider_x509_cert_url: 'XXXX',
+	client_x509_cert_url: 'XXXX'
+};
